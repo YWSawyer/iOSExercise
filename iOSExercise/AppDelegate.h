df0780fd020f8c4b2b60f6a4b1755be73b7cb1b7
wei.yuan <yuan_wei_1@163.com>
2018-02-26 21:03:03 +0800

    first import

---
+++ iOSExercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSExercise
+//
+//  Created by wei.yuan on 26/02/2018.
+//  Copyright © 2018 wei.yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
